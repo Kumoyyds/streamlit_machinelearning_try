93e0e12b32ab7ba773a819f3481a134d4d5dd938
Kumoyyds <144432174+Kumoyyds@users.noreply.github.com>
2024-09-19 16:19:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,9 @@ with st.expander('Data'):
   st.write('**Raw Data**') # using the markdown here
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
+  st.write('**X**')   
+  X = df.drop('species', axis=1)
+  X
+  st.write('**Y**')
+  Y = df[['species']]
+  Y
