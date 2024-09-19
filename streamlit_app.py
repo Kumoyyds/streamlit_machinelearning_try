8cae9784899d47c39b53346edf59a5efad619eeb
Kumoyyds <144432174+Kumoyyds@users.noreply.github.com>
2024-09-19 16:13:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 ml app')
 st.info('this is a machine learning mapp')
 
 st.write('Hello world!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
