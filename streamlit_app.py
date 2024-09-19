473d856a1a694bd099dffa85421a99d3d3c67447
Kumoyyds <144432174+Kumoyyds@users.noreply.github.com>
2024-09-19 16:44:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,21 @@ with st.sidebar:
   gender = st.selectbox('Gender', ('male', 'female'))
   
   # kinda like an option of range 
+  # the third value is the default value lol
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+
+  # the dataframe for the input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0]) 
+  input_df
+
+  
 
