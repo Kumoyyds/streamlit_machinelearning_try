97eb58471778538ff540473f3c4716a8f2810a62
Kumoyyds <144432174+Kumoyyds@users.noreply.github.com>
2024-09-19 16:36:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,14 @@ with st.expander('Data visualization'):
   # referring to https://docs.streamlit.io/develop/api-reference/charts/st.scatter_chart please
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
+
+# more data preparation
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
+  # kinda like an option of range 
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+
